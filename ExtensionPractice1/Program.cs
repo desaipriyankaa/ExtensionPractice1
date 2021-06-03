d9946e10b9dd82a995608bc108c191483d740795
desaipriyankaa <priyanka.desai@klingelnberg.com>
2021-06-03 09:44:11 +0530

    Add project files.

---
+++ ExtensionPractice1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace ExtensionPractice1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string demo = "This is a demo";
+            demo.PrintToConsole();
+        }
+    }
+
+    public static class extensions
+    {
+        public static void PrintToConsole(this string message)
+        {
+            Console.WriteLine(message);
+        }
+    }
+}
